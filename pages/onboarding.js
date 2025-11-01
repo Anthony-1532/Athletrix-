5635f168c81f492aae26127de936ca25719b068a
Anthony-1532 <Anthonyaboki007@gmail.com>
2025-11-01 12:41:36 +0100

    Create pages/onboarding.js

---
+++ pages/onboarding.js
@@ -0,0 +1,5 @@
+import AthleteOnboarding from '../components/AthleteOnboarding';
+
+export default function Onboarding() {
+  return <AthleteOnboarding isOpen={true} onClose={() => {}} onComplete={() => {}} />;
+}
